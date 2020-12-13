08f3d7f544170912c032eaf62aab8c2176af81a3
FauziZone <ahmadfauzirahman99@gmail.com>
2020-12-13 13:01:23 +0700

    first commit

---
+++ src/assets/js/custom.js
@@ -0,0 +1,64 @@
+function menuUmum() {
+    location = "1.pilih-poli-tujuan.html";
+    // alert('umum');
+}
+
+function menuRujukan() {
+    location = "2.rujukan-baru.html";
+    // alert('rujukan');
+}
+
+function menuKontrol() {
+    location = "3.kontrol.html";
+    // alert('Kontrol');
+}
+
+function menuBeranda() {
+    location = "beranda.html";
+    // alert('Beranda');
+}
+
+function keKonfirmasiUmum() {
+    location = "1.1.konfirmasi-umum.html";
+    // alert('Beranda');
+}
+
+function kePilihRujukan() {
+    location = "2.1.pilih-rujukan.html";
+    // alert('Beranda');
+}
+
+function kePilihPoliRujukan() {
+    location = "2.2.pilih-poli-tujuan.html";
+    // alert('Beranda');
+}
+
+function keKonfirmasiRujukan() {
+    location = "2.3.konfirmasi-rujukan.html";
+    // alert('Beranda');
+}
+
+function keKonfirmasiRujukan() {
+    location = "2.3.konfirmasi-rujukan.html";
+    // alert('Beranda');
+}
+
+function kePilihPoliKontrol() {
+    location = "3.1.pilih-poli-tujuan.html";
+    // alert('Beranda');
+}
+
+function keKonfirmasiKontrol() {
+    location = "3.2.konfirmasi-kontrol.html";
+    // alert('Beranda');
+}
+
+function login() {
+    location = "beranda.html";
+    // alert('Beranda');
+}
+
+function logout() {
+    location = "index.html";
+    // alert('Beranda');
+}
